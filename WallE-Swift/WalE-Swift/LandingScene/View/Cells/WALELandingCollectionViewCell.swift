31b9b4af5147c415bc13ac8f6a95ff4e8b1a4695
Abhishek Banerjee <abhishekbanerjee@Abhisheks-MacBook-Pro.local>
2022-04-13 22:57:03 +0530

    added readme.md + width scrolling for image cell

---
+++ WallE-Swift/WalE-Swift/LandingScene/View/Cells/WALELandingCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  WALELandingCollectionViewCell.swift
+//  WalE-Swift
+//
+//  Created by Abhishek Banerjee on 13/04/22.
+//
+
+import UIKit
+
+class WALELandingCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
